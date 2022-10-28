6ae22e831e33944134615995746f5f1034f759eb
miraejang <mirae611@nate.com>
2022-10-28 17:53:38 +0900

    BaseComponent로 items 생성

--- src/app.ts
+++ src/app.ts
@@ -7,54 +7,53 @@ class App {
 
     // ul 생성
     const page = new BaseComponent('<ul class="items"></ul>');
-    page.attachTo(main, 'afterbegin');
+    page.attachTo(main, 'beforeend');
 
     const ul = main.querySelector('.items')! as HTMLUListElement;
 
     // li 생성 - image
-    const imageTemp = document.createElement('template');
-    imageTemp.innerHTML = `<li class="item">
-                              <div class="item__wrap">
-                                <div class="item__body"></div>
-                                <div class="item__controls">
-                                  <button class="close-button">&times;</button>
-                                </div>
-                              </div>
-                            </li>`;
-    const imageLi = imageTemp.content.firstElementChild! as HTMLLIElement;
-    ul.insertAdjacentElement('beforeend', imageLi);
+    const imageTemp = new BaseComponent(`<li class="item">
+                                      <div class="item__wrap">
+                                        <div class="item__body"></div>
+                                        <div class="item__controls">
+                                          <button class="close-button">&times;</button>
+                                        </div>
+                                      </div>
+                                    </li>`);
+    imageTemp.attachTo(ul, 'beforeend');
+    const imageLi = imageTemp.element;
 
     const imageWrap = imageLi.querySelector('.item__wrap')! as HTMLElement;
     imageWrap.classList.add('item__preview', 'item__image');
     const imageLibody = imageLi.querySelector('.item__body')! as HTMLElement;
+
     const imageBody = document.createElement('template');
     imageBody.innerHTML = `<div class='item__container'>
-                              <div class="item__view-box">
-                                <img
-                                  src="https://picsum.photos/800/400"
-                                  alt="title"
-                                  class="item__view"
-                                />
-                              </div>
-                              <div class="item__title-box">
-                                <h2 class="item__title">Image</h2>
-                              </div>
-                            </div>`;
+      <div class="item__view-box">
+        <img
+          src="https://picsum.photos/800/400"
+          alt="title"
+          class="item__view"
+        />
+      </div>
+      <div class="item__title-box">
+        <h2 class="item__title">Image</h2>
+      </div>
+    </div>`;
     const image = imageBody.content.firstElementChild! as HTMLLIElement;
     imageLibody.insertAdjacentElement('afterbegin', image);
 
     // li 생성 - video
-    const videoTemp = document.createElement('template');
-    videoTemp.innerHTML = `<li class="item">
-                              <div class="item__wrap">
-                                <div class="item__body"></div>
-                                <div class="item__controls">
-                                  <button class="close-button">&times;</button>
-                                </div>
-                              </div>
-                            </li>`;
-    const videoLi = videoTemp.content.firstElementChild! as HTMLLIElement;
-    ul.insertAdjacentElement('beforeend', videoLi);
+    const videoTemp = new BaseComponent(`<li class="item">
+                                          <div class="item__wrap">
+                                            <div class="item__body"></div>
+                                            <div class="item__controls">
+                                              <button class="close-button">&times;</button>
+                                            </div>
+                                          </div>
+                                        </li>`);
+    videoTemp.attachTo(ul, 'beforeend');
+    const videoLi = videoTemp.element;
 
     const videoWrap = videoLi.querySelector('.item__wrap')! as HTMLElement;
     videoWrap.classList.add('item__preview', 'item__video');
@@ -78,20 +77,19 @@ class App {
                               </div>
                             </div>`;
     const video = videoBody.content.firstElementChild! as HTMLLIElement;
-    videoLibody.insertAdjacentElement('afterbegin', video);
+    videoLibody.insertAdjacentElement('beforeend', video);
 
     // li 생성 - note
-    const noteTemp = document.createElement('template');
-    noteTemp.innerHTML = `<li class="item">
-                              <div class="item__wrap">
-                                <div class="item__body"></div>
-                                <div class="item__controls">
-                                  <button class="close-button">&times;</button>
-                                </div>
-                              </div>
-                            </li>`;
-    const noteLi = noteTemp.content.firstElementChild! as HTMLLIElement;
-    ul.insertAdjacentElement('beforeend', noteLi);
+    const noteTemp = new BaseComponent(`<li class="item">
+                                          <div class="item__wrap">
+                                            <div class="item__body"></div>
+                                            <div class="item__controls">
+                                              <button class="close-button">&times;</button>
+                                            </div>
+                                          </div>
+                                        </li>`);
+    noteTemp.attachTo(ul, 'beforeend');
+    const noteLi = noteTemp.element;
 
     const noteWrap = noteLi.querySelector('.item__wrap')! as HTMLElement;
     noteWrap.classList.add('item__basic', 'item__note');
@@ -109,17 +107,16 @@ class App {
     noteLibody.insertAdjacentElement('afterbegin', note);
 
     // li 생성 - todo
-    const todoTemp = document.createElement('template');
-    todoTemp.innerHTML = `<li class="item">
-                              <div class="item__wrap">
-                                <div class="item__body"></div>
-                                <div class="item__controls">
-                                  <button class="close-button">&times;</button>
-                                </div>
-                              </div>
-                            </li>`;
-    const todoLi = todoTemp.content.firstElementChild! as HTMLLIElement;
-    ul.insertAdjacentElement('beforeend', todoLi);
+    const todoTemp = new BaseComponent(`<li class="item">
+                                          <div class="item__wrap">
+                                            <div class="item__body"></div>
+                                            <div class="item__controls">
+                                              <button class="close-button">&times;</button>
+                                            </div>
+                                          </div>
+                                        </li>`);
+    todoTemp.attachTo(ul, 'beforeend');
+    const todoLi = todoTemp.element;
 
     const todoWrap = todoLi.querySelector('.item__wrap')! as HTMLElement;
     todoWrap.classList.add('item__basic', 'item__todo');
